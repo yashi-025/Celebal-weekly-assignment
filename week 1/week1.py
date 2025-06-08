f68f0c77a59ccfe3d4a2999d872e8eb45e19afc2
Yashika Garg <146928623+yashi-025@users.noreply.github.com>
2025-06-08 14:53:23 +0530

    Update week1.py

--- week 1/week1.py
+++ week 1/week1.py
@@ -1,32 +1,31 @@
 def lower_triangle(n):
-    print("LOWER TRIANGLE: ")
+    print("\nLOWER TRIANGLE: ")
     for i in range(n):
         for j in range(i+1):
             print("*", end = "")
         print()       
     
 def upper_triangle(n):
-    print("UPPER TRIANGLE: ")
+    print("\nUPPER TRIANGLE: ")
     for i in range(n):
-        for j in range(n+1, i+1, -1):
+        for j in range(n, i, -1):
             print("*", end = "")
         print()
         
 def pyramid(p):
-    print("PYRAMID: ")
-    for i in range((p//2) + 1):
-        for j in range((p//2) -i):
+    print("\nPYRAMID: ")
+    for i in range(p):
+        #print spaces
+        for j in range(p - i):
             print(" ", end = "")
-        for k in range((p//2)-i, (p//2)+i+1):
+        # print stars
+        for k in range(2 * i + 1):
             print("*", end = "")   
         print()                  
 
-
 if __name__ == "__main__":
-    length = int(input("Enter required length of triangles: "))
-    upper_triangle(length)
-    lower_triangle(length)
-    print(f'''\n# Note: if length is even pyramid is of length+1 
-          given length: {length}''')
-    pyramid(length)
+    rows = int(input("Enter required no. of rows: "))
+    upper_triangle(rows)
+    lower_triangle(rows)
+    pyramid(rows)
             
